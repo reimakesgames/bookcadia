4423a79dc16dd01949249105385573071a308851
rei <reimakesgames.dev@gmail.com>
2024-11-25 12:17:16 +0800

    reader

--- public/js/home.ts
+++ public/js/home.ts
@@ -1,1 +1,17 @@
-console.log("Hello, world!")
+function createClickEventsForBooks() {
+	const carousel = document.getElementById("carousel")
+
+	if (!carousel) {
+		return
+	}
+
+	// loop through the carousel items and create an event that goes to api/v1/reader/:id
+	for (let i = 0; i < carousel.children.length; i++) {
+		carousel.children[i]?.addEventListener("click", () => {
+			const bookId = carousel.children[i]?.id.split("-")[1]
+			window.location.href = `/reader?book=${bookId}`
+		})
+	}
+}
+
+createClickEventsForBooks()
