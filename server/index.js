726d29199746f45640045271a444d42b69baef4e
rei <reimakesgames.dev@gmail.com>
2024-11-28 15:00:50 +0800

    remove lag middleware lol

--- server/index.js
+++ server/index.js
@@ -4,11 +4,6 @@ const dotenv = await import("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
-// lag middleware just to simulate a slow network
-app.use((req, res, next) => {
-    const lag = Math.floor(Math.random() * 200) + 20;
-    setTimeout(next, lag);
-});
 app.use("/", express.static("public"));
 app.use("/api/v1/books", Book);
 app.listen(PORT, () => {
